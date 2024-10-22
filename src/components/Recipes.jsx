640fb8137815d56345e854ca0552b1c9c3c643fa
xyryc <amdanikzz@gmail.com>
2024-10-23 02:16:35 +0600

    ingredients rendered, minor tweaks

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -11,43 +11,60 @@ const Recipes = () => {
   }, []);
 
   return (
-    <div className="md:w-2/3 font-fira-sans">
-      {recipes.map((recipe) => (
-        <div key={recipe.recipe_id} className="border rounded-2xl p-6">
-          {/* <img
-            className="w-[330px] h-[200px] object-cover rounded-2xl"
-            src={recipe.recipe_image}
-            alt=""
-          /> */}
+    <div className="md:w-2/3 font-fira-sans mt-12">
+      <div className="grid lg:grid-cols-2 gap-6">
+        {recipes.map((recipe) => (
+          <div key={recipe.recipe_id} className="border rounded-2xl p-6">
+            <img
+              className="w-full h-[200px] object-cover rounded-2xl"
+              //   src={recipe.recipe_image}
+              alt={`recipe image of ${recipe.recipe_name}`}
+            />
 
-          {/* title */}
-          <div className="my-6">
-            <p className="font-lexend text-xl font-semibold">
-              {recipe.recipe_name}
-            </p>
-            <p className=" my-4">{recipe.short_description}</p>
-            <hr />
-          </div>
-
-          {/* ingredients container */}
-          <div className="mb-4">
-            <h3 className="font-lexend font-medium text-lg">Ingredients: </h3>
-          </div>
-
-          {/* bottom container */}
-          <div>
-            <hr />
+            {/* title */}
             <div className="my-6">
-              <span>Time</span>
-              <span>Calories</span>
+              <p className="font-lexend text-xl font-semibold">
+                {recipe.recipe_name}
+              </p>
+              <p className=" text-[#878787] my-4">{recipe.short_description}</p>
+              <hr />
             </div>
-          </div>
 
-          <button className="btn rounded-[50px] bg-[#0BE58A] border-none font-lexend">
-            Want to Cook
-          </button>
-        </div>
-      ))}
+            {/* ingredients container */}
+            <div className="mb-4">
+              <h3 className="font-lexend font-medium text-lg mb-4">
+                Ingredients: {recipe.ingredients.length}
+              </h3>
+              <ul className="list-disc list-inside  text-[#878787]">
+                {recipe.ingredients.map((item, index) => (
+                  <li className="" key={index}>
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* bottom container */}
+            <div>
+              <hr />
+              <div className="my-6 text-gray-700">
+                <span className="mr-4">
+                  <i className="fa-regular fa-clock mr-2"></i>
+                  <span>{recipe.preparing_time} minutes</span>
+                </span>
+                <span> 
+                <i className="fa-solid fa-fire-flame-curved mr-2"></i>
+                <span>{recipe.calories}</span>
+                </span>
+              </div>
+            </div>
+
+            <button className="btn rounded-[50px] bg-[#0BE58A] border-none font-lexend">
+              Want to Cook
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 const Sidebar = () => {
   return (
-    <div className="w-1/3 ">
+    <div className="w-1/3 mt-12">
       <h2>Sidebar </h2>
     </div>
   );
